09ea0afc85d35bcc29428091c6c2e5bf738589f9
Aman <foraman202514@gmail.com>
2026-08-16 00:11:03 +0530

    Added Arithmetic Operator Program

---
+++ 02_ElementaryProgramming/_04ArithmeticOperator/_01ArithmeticOperator.py
@@ -0,0 +1,24 @@
+"""
+In python, arithmetic operator are used to perform mathematical calculations.
+
+    | Operator | Meaning             |
+    | -------- | ------------------- |
+    | `+`      | Addition            |
+    | `-`      | Subtraction         |
+    | `*`      | Multiplication      |
+    | `/`      | Division            |
+    | `//`     | Floor division      |
+    | `%`      | Modulus (remainder) |
+    | `**`     | Exponentiation      |
+"""
+
+a = 5
+b = 4
+
+print(a + b) # output: 9
+print(a - b) # output: 1
+print(a * b) # output: 20
+print(a / b) # output: 1.25
+print(a % b) # output: 1
+print(a // b) # output: 1
+print(a ** b) # output: 625
