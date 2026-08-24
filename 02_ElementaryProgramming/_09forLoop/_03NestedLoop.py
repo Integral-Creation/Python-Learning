e1ba43f306753c7b543d3130b4895c94759ba51f
Aman <foraman202514@gmail.com>
2026-08-24 22:05:36 +0530

    Added Nested Loop Python Program

---
+++ 02_ElementaryProgramming/_09forLoop/_03NestedLoop.py
@@ -0,0 +1,18 @@
+"""
+    Nested Loop
+        a loop within another loop
+"""
+
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end= " ")
+    print()
+
+"""
+Output:
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * * 
+"""
