c06cf668666d5172a12374a44772fcab92c9d003
Aman <foraman202514@gmail.com>
2026-08-30 22:30:50 +0530

    Added Function Python Program

---
+++ 04_Function/_01Intro/_02function.py
@@ -0,0 +1,22 @@
+# simple calculator
+
+def add(a, b):
+    print(a + b)
+
+def sub(a, b):
+    print(a - b)
+
+def mul(a, b):
+    print(a * b)
+
+def div(a, b):
+    print(a / b)
+
+def modulus(a, b):
+    print(a % b)
+
+add(1,3)
+sub(4,2)
+mul(4,5)
+div(5,5)
+modulus(3,4)
